2f2fc13c7474e01f04daeba36a1398a278927762
Rico Sta. Cruz <rico@ambiescent.com>
2011-08-22 06:15:45 +0800

    Update the Ruby recipe to use HTTP/targz.

--- data/ruby19.sh
+++ data/ruby19.sh
@@ -9,8 +9,8 @@ sudo apt-get -y install libc6-dev libssl-dev libmysql++-dev libsqlite3-dev make 
 status "Downloading Ruby..."
 mkdir -p /tmp/src
 cd /tmp/src
-wget ftp://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.2-p180.zip
-unzip *.zip
+wget http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.2-p180.tar.gz
+tar -xzvf ruby-1.9.2-p180.tar.gz
 
 installing "Ruby"
 cd ruby-1.9.2-p180
