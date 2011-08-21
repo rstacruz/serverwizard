d76504af85ac7b44d1c5590de76e2ecb844670f3
Rico Sta. Cruz <rico@ambiescent.com>
2011-08-22 06:31:27 +0800

    Change permissions for user recipe's authorized_keys file.

--- data/user.sh
+++ data/user.sh
@@ -16,5 +16,5 @@ status "Setting up SSH keys..."
 sudo mkdir /home/$MY_USER/.ssh
 echo $SSH_PUBKEY > /home/$MY_USER/.ssh/authorized_keys
 sudo chmod 700 /home/$MY_USER/.ssh
-sudo chmod 400 /home/$MY_USER/.ssh/authorized_keys
+sudo chmod 600 /home/$MY_USER/.ssh/authorized_keys
 sudo chown -R $MY_USER:$MY_USER /home/$MY_USER/.ssh
