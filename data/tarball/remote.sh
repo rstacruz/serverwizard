91b58bc9035148dc4579d1be5566da3d740a3e90
Rico Sta. Cruz <rico@ambiescent.com>
2011-08-22 23:00:37 +0800

    Better remote.sh messages.

--- data/tarball/remote.sh
+++ data/tarball/remote.sh
@@ -5,11 +5,11 @@ if [ $# == 0 ]; then
     exit 65
 fi
 
-echo "Connecting to $1..."
-
 # Copy all files in the current path to the remote server.
+echo "Copying files to $1..."
 TMP_PATH="/tmp/bootstrap-sh-`date +%s`"
 tar -zc . | ssh $* -- "mkdir -p $TMP_PATH; cd $TMP_PATH; tar -zxf -"
 
 # Execute bootstrap.sh in the new remote path, then clean up when done.
+echo "Running the bootstrap script in $1..."
 ssh -t $* -- "cd $TMP_PATH && sudo bash < bootstrap.sh; rm -rf $TMP_PATH"
