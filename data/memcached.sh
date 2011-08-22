d2d3f5bb8a5e0ff036ecd8f3c1ee9d1ccde083fc
Rico Sta. Cruz <rico@ambiescent.com>
2011-08-22 14:15:24 +0800

    Add memcached and postgresql.

---
+++ data/memcached.sh
@@ -0,0 +1,6 @@
+# name: Memcache
+
+ensure_updated_apt
+
+installing "Memcache"
+apt-get install -y memcached libmemcache-dev
---
+++ data/postgresql.sh
@@ -0,0 +1,6 @@
+# name: PostgreSQL
+
+ensure_updated_apt
+
+installing "PostgreSQL"
+apt-get install -y postgresql
