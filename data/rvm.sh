1831beacbfdf6cd4acb1ee760067cad2db1f8813
Rico Sta. Cruz <rico@ambiescent.com>
2011-08-22 13:48:24 +0800

    Fix RVM script to install Ruby properly.

--- data/rvm.sh
+++ data/rvm.sh
@@ -9,12 +9,13 @@ apt-get install -y build-essential bison openssl libreadline6 libreadline6-dev c
 status "Installing system-wide RVM"
 bash < <(curl -s https://rvm.beginrescueend.com/install/rvm)
 
+# Add yourself to RVM
 status "Installing Ruby via RVM"
-rvm install ruby-1.9.2-p180
+bash -l -c "rvm install ruby-1.9.2-p180"
 
 if [ -n "$MY_USER" ]; then
   status "Giving $MY_USER RVM rights"
-  sudo adduser $MY_USER rvm
+  adduser $MY_USER rvm
 fi
 
 status "RVM is installed."
