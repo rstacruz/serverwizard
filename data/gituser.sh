353471b97271d652a5365b8c97e535606b50ce15
Rico Sta. Cruz <rico@ambiescent.com>
2011-08-22 11:47:50 +0800

    Fix the gituser recipe.

--- data/gituser.sh
+++ data/gituser.sh
@@ -7,7 +7,7 @@ useradd git --home /home/git --create-home --shell /usr/bin/git-shell
 if [ -n "$SSH_PUBKEY" ]; then
   status "Setting up SSH keys..."
   mkdir /home/git/.ssh
-  echo $SSH_PUBKEY > /home/$MY_USER/.ssh/authorized_keys
+  echo $SSH_PUBKEY > /home/git/.ssh/authorized_keys
   chmod 700 /home/git/.ssh
   chmod 400 /home/git/.ssh/authorized_keys
   chown -R git:git /home/git/.ssh
