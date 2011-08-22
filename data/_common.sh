54ae41990ab45b37ed1905f1abf7a70c40d9e66b
Rico Sta. Cruz <rico@ambiescent.com>
2011-08-22 12:02:07 +0800

    Add better error messages.

--- data/_common.sh
+++ data/_common.sh
@@ -15,6 +15,15 @@ status() {
     echo -e "\033[0;32m*** $*\033[0;m"
 }
 
+err() {
+    echo -e "\033[0;31m* Error: $*\033[0;m"
+}
+
+die() {
+  err "$*"
+  exit 256
+}
+
 installing() {
     echo -e "\033[0;32m*** Installing: $*...\033[0;m"
 }
@@ -31,27 +40,24 @@ ensure_updated_apt() {
   if [ "$UPDATED_APT" != "1" ]; then
     status "Updating apt cache..."
     UPDATED_APT=1
-    apt-get update
+    apt-get update || (die "Failed to update.")
   fi
 }
 
 # Ensure Linux-ness
 if [ ! -f "/etc/lsb-release" ]; then
-  echo "Error: This platform is not supported."
-  echo "Please try this on an Ubuntu 10.04 server instead."
-  exit 256
+  err "This platform is not supported."
+  die "Please try this on an Ubuntu 10.04 server instead."
 fi
 
 # Ensure Ubuntu-ness
 source /etc/lsb-release
 if [ "$DISTRIB_ID" != "Ubuntu" ]; then
-  echo "Error: This platform is not supported."
-  echo "Please try this on an Ubuntu 10.04 server instead."
-  exit 256
+  err "Error: This platform is not supported."
+  die "Please try this on an Ubuntu 10.04 server instead."
 fi
 
 # Ensure root-ness
 if [ `whoami` != "root" ]; then
-    echo "Run me as root!"
-    exit
+    die "Run me as root!"
 fi
