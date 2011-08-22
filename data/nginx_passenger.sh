83010a9f48be545317aa0be4fcee0012fe243179
Rico Sta. Cruz <rico@ambiescent.com>
2011-08-22 11:31:25 +0800

    Remove excess comments on the nginx recipe.

--- data/nginx_passenger.sh
+++ data/nginx_passenger.sh
@@ -1,14 +1,12 @@
 # name: Nginx with Passenger 3
 # description: Compiles Nginx from source.
 
-# Install
 installing "Passenger gem"
 sudo gem install passenger -v 3.0.6
 
 installing "Nginx via Passenger"
 passenger-install-nginx-module --auto --auto-download --prefix=/opt/nginx
 
-# Yeah
 installing "Nginx service script"
 cat_file nginx/nginx > /etc/init.d/nginx
 chown root:root /etc/init.d/nginx
