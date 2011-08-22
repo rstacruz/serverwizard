8b79ab6ab4b4d814c95377faeada080fde961d23
Rico Sta. Cruz <rico@ambiescent.com>
2011-08-22 22:47:43 +0800

    .

--- data/tarball/remote.sh
+++ data/tarball/remote.sh
@@ -7,6 +7,9 @@ fi
 
 echo "Connecting to $1..."
 
+# Copy all files in the current path to the remote server.
 TMP_PATH="/tmp/bootstrap-sh-`date +%s`"
 tar -zc . | ssh $* -- "mkdir -p $TMP_PATH; cd $TMP_PATH; tar -zxf -"
+
+# Execute bootstrap.sh in the new remote path, then clean up when done.
 ssh -t $* -- "cd $TMP_PATH && sudo bash < bootstrap.sh; rm -rf $TMP_PATH"
