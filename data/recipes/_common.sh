32247b497d71a0d1d98f452cab61ef3488c3ba40
Rico Sta. Cruz <rico@ambiescent.com>
2011-08-23 01:12:37 +0800

    Shorten helpers.

--- data/recipes/_common.sh
+++ data/recipes/_common.sh
@@ -3,33 +3,15 @@
 DIR="`pwd`"
 
 # Helpers
-status() {
-  echo -e "\033[0;32m*** $*\033[0;m"
-}
-status_() {
-  echo -e "\033[0;32m    $*\033[0;m"
-}
-err() {
-  echo -e "\033[0;31m* Error: $*\033[0;m"
-}
-die() {
-  err "$*"; exit 256
-}
-installing() {
-  echo -e "\033[0;32m*** Installing: $*...\033[0;m"
-}
+status()     { echo -e "\033[0;32m*** $*\033[0;m"; }
+status_()    { echo -e "\033[0;32m    $*\033[0;m"; }
+err()        { echo -e "\033[0;31mERROR: $*\033[0;m"; }
+err_()       { echo -e "\033[0;31m       $*\033[0;m"; }
+installing() { echo -e "\033[0;32m*** Installing: $*...\033[0;m"; }
+die()        { err "$*"; exit 256; }
 
-cat_file() {
-  if [ -f "$DIR/$*" ]; then
-    cat "$DIR/$*"
-  else
-    wget "http://HTTP_HOST/$*" -q -O -
-  fi
-}
-
-source_file() {
-  cat_file $* | source /dev/stdin
-}
+cat_file()    { [ -f "$DIR/$*" ] && cat "$DIR/$*" || wget "http://HTTP_HOST/$*" -q -O -; }
+source_file() { cat_file $* | source /dev/stdin; }
 
 ensure_updated_apt() {
   if [ "$UPDATED_APT" != "1" ]; then
@@ -41,20 +23,23 @@ ensure_updated_apt() {
 
 # Ensure Linux-ness
 if [ ! -f "/etc/lsb-release" ]; then
-  err "Only Linux platforms are supported."
-  die "Please try this on an Ubuntu Server 10.04+ instance."
+  err  "Only Linux platforms are supported."
+  err_ "Please try this on an Ubuntu Server 10.04+ instance."
+  exit 256
 fi
 
 # Ensure Ubuntu-ness
 source /etc/lsb-release
 if [ "$DISTRIB_ID" != "Ubuntu" ]; then
-  err "Error: This Linux distribution is not supported."
-  die "Please try this on an Ubuntu Server 10.04+ instance."
+  err  "Error: This Linux distribution is not supported."
+  err_ "Please try this on an Ubuntu Server 10.04+ instance."
+  exit 256
 fi
 
 if [ "$(echo "$DISTRIB_RELEASE < 10.04" | bc)" == "1" ]; then
-  err "Error: Your Ubuntu version is too old."
-  die "Please try this on an Ubuntu Server 10.04+ instance."
+  err  "Error: Your Ubuntu version is too old."
+  err_ "Please try this on an Ubuntu Server 10.04+ instance."
+  exit 256
 fi
 
 # Ensure root-ness
