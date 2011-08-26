438153dc3f7516c9cde1aa6e13c3ec9078652917
Rico Sta. Cruz <rico@ambiescent.com>
2011-08-26 18:08:46 +0800

    Add Cassandra.

---
+++ data/recipes/cassandra.sh
@@ -0,0 +1,17 @@
+# name: Cassandra 0.8
+# description: From Apache.org's deb sources.
+
+status "Adding Apache.org's apt source for Cassandra..."
+(
+    echo "deb http://www.apache.org/dist/cassandra/debian 08x main"
+    echo "deb-src http://www.apache.org/dist/cassandra/debian 08x main"
+) > /etc/apt/sources.list.d/cassandra.list
+
+gpg --keyserver pgp.mit.edu --recv-keys 4BD736A82B5C1B00
+gpg --export --armor 4BD736A82B5C1B00 | apt-key add -
+
+installing "Cassandra"
+
+apt-get update
+apt-get install -y cassandra
+
