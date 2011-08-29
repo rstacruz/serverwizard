9949ae8b6136818aa314ccd618bbd84ccd0ebf46
Rico Sta. Cruz <rico@ambiescent.com>
2011-08-30 03:13:39 +0800

    Make the NodeJS recipe compile from source.

--- data/recipes/nodejs.sh
+++ data/recipes/nodejs.sh
@@ -1,14 +1,23 @@
 # name: NodeJS with NPM
+# description: Latest, compiled from source.
 # notes: |
 #  - You may need to type *"yes"* somewhere during the installation of NPM.
 # needs:
 #   - _apt-update
+#  - _build-essential
+
+apt-get install -y libssl-dev
 
 installing "NodeJS"
-apt-get install -y python-software-properties curl
-add-apt-repository ppa:chris-lea/node.js
-apt-get update
-apt-get install -y nodejs
+status "Downloading NodeJS..."
+mkdir -p /tmp/nodejs-src
+cd /tmp/nodejs-src
+wget http://nodejs.org/dist/node-latest.tar.gz
+tar zxf node-latest.tar.gz --strip-components=1
+
+status "Building NodeJS..."
+./configure --prefix=/usr/local
+make install
 
 installing "Node Package Manager"
 wget http://npmjs.org/install.sh -q -O - | sudo sh
