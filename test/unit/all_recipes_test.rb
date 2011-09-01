c885a597e1d748a9216860f0d54ee17341bf9924
Rico Sta. Cruz <rico@ambiescent.com>
2011-09-01 23:37:12 +0800

    Robust tests.

--- test/unit/all_recipes_test.rb
+++ test/unit/all_recipes_test.rb
@@ -1,10 +1,29 @@
 require_relative '../test_helper'
 
 class AllRecipesTest < UnitTest
-  Dir["#{Recipe.home}/*.sh"].each { |fn|
-    fn = File.basename(fn, '.sh')
-    test(fn) { Recipe[fn].meta }
-  }
+  def self.each_recipe
+    Dir["#{Recipe.home}/*.sh"].each { |fname| yield File.basename(fname, '.sh'), fname }
+  end
+
+  each_recipe do |name, fn|
+    describe(name) {
+      setup { @recipe = Recipe[name] }
+
+      test "Parsing of YAML metadata" do
+        @recipe.meta
+      end
+      
+      test "Dependencies" do
+        deps = @recipe.meta.dependencies
+
+        if deps
+          deps.map! { |str| Recipe[str] }
+          deps.should.not.include nil
+          @recipe.dependencies
+        end
+      end
+    }
+  end
 
   test "all" do
     Recipe.all
