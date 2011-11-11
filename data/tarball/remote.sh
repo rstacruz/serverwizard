84a51c2846ad482e1980ca53c2f11db4df476f3c
Rico Sta. Cruz <rico@ambiescent.com>
2011-11-11 17:43:20 +0800

    Fix remote.sh.

--- data/tarball/remote.sh
+++ data/tarball/remote.sh
@@ -10,8 +10,8 @@ fi
 # Copy all files in the current path to the remote server.
 echo "Copying files to $1..."
 TMP_PATH="~/bootstrap-sh-`date +%s`$RANDOM"
-tar -zc . | ssh $* -- "mkdir -p $TMP_PATH; cd $TMP_PATH; tar zx"
+tar -zc . | ssh $1 -- "mkdir -p $TMP_PATH; cd $TMP_PATH; tar zx"
 
 # Execute bootstrap.sh in the new remote path, then clean up when done.
 echo "Running $FILE on $1..."
-ssh -t $* -- "cd $TMP_PATH && sudo bash < $FILE; rm -rf $TMP_PATH"
+ssh -t $1 -- "cd $TMP_PATH && sudo bash < $FILE; rm -rf $TMP_PATH"
