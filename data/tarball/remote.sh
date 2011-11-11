4f7bc04b5c93ee0ef8450de489ddc795c5d9aa2e
Rico Sta. Cruz <rico@ambiescent.com>
2011-11-11 17:38:34 +0800

    Update remote.sh to support ./remote.sh foo@host FILE=xxx.

--- data/tarball/remote.sh
+++ data/tarball/remote.sh
@@ -5,11 +5,13 @@ if [ $# == 0 ]; then
     exit 65
 fi
 
+[ -z "$FILE" ] && FILE="bootstrap.sh"
+
 # Copy all files in the current path to the remote server.
 echo "Copying files to $1..."
-TMP_PATH="/tmp/bootstrap-sh-`date +%s`$RANDOM"
-tar -zc . | ssh $* -- "mkdir -p $TMP_PATH; cd $TMP_PATH; tar -zxf -"
+TMP_PATH="~/bootstrap-sh-`date +%s`$RANDOM"
+tar -zc . | ssh $* -- "mkdir -p $TMP_PATH; cd $TMP_PATH; tar zx"
 
 # Execute bootstrap.sh in the new remote path, then clean up when done.
-echo "Running the bootstrap script in $1..."
-ssh -t $* -- "cd $TMP_PATH && sudo bash < bootstrap.sh; rm -rf $TMP_PATH"
+echo "Running $FILE on $1..."
+ssh -t $* -- "cd $TMP_PATH && sudo bash < $FILE; rm -rf $TMP_PATH"
